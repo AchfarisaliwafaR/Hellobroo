e2ee35cc361314fba3b3c4c04070ac84b7b8382d
aliwafa <faizarwafa@gmail.com>
2024-03-14 21:10:04 +0700

    tambah baris kedua

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -16,7 +16,7 @@ public class Hello {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
-        System.out.println("tambah baris ke dua");
+        System.out.println("tambah baris kedua");
         
     }
     
